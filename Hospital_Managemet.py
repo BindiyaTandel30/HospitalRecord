066d480b0ea6dbd4cd614ad78658bbc3547ed0dc
Shubham Namjoshi <bindiyatandel3009@gmail.com>
2021-08-22 01:05:49 +0530

    created tables for induvidual country

--- Hospital_Managemet.py
+++ Hospital_Managemet.py
@@ -4,13 +4,25 @@ import mysql.connector as sqlc
 mydb=sqlc.connect(
     host="localhost",
     user="root",
-    password="")
+    password="",
+    database="customer_record")
 
 cur=mydb.cursor()
-s="CREATE OR REPLACE DATABASE customer_record"
-cur.execute(s)
+##s="CREATE OR REPLACE DATABASE customer_record"
+##cur.execute(s)
 
 #read data
 f=open("customer_record.txt","r")
 for line in f:
     data=line.split("|")
+    if(data[1]=="D"):
+        country=data[9]
+        try:
+            s="CREATE TABLE {}(Customer_Name VARCHAR(255),Customer_ID VARCHAR(18) PRIMARY KEY,Customer_Open_Date DATE,Last_Consulted_Date DATE,Vaccination_Type CHAR(5),Doctor_Consulted CHAR(255),State CHAR(5),Country CHAR(5),Post_Code INT(5),Date_of_Birth DATE,Active_Customer CHAR(1))".format(country)
+            cur.execute(s)
+            mydb.commit()
+        except:
+            print()
+        
+            
+f.close()
