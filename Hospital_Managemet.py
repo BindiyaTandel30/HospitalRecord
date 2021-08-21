eec0f884c5753139eeaf6dc0d851e75f7984e16a
Shubham Namjoshi <bindiyatandel3009@gmail.com>
2021-08-22 01:10:41 +0530

    converted code into methods

--- Hospital_Managemet.py
+++ Hospital_Managemet.py
@@ -1,41 +1,56 @@
 import mysql.connector as sqlc
 
-mydb=sqlc.connect(
-    host="localhost",
-    user="root",
-    password=""
-    )
+def connect_sql():
+    mydb=sqlc.connect(
+        host="localhost",
+        user="root",
+        password=""
+        )
+    return mydb
 
-cur=mydb.cursor()
-s="CREATE OR REPLACE DATABASE customer_record"
-cur.execute(s)
+def create_database(mydb):
+    cur=mydb.cursor()
+    s="CREATE OR REPLACE DATABASE customer_record"
+    cur.execute(s)
 
-s="USE customer_record"
-cur.execute(s)
+    s="USE customer_record"
+    cur.execute(s)
+    return cur
 
-#open text file for read data in read only mode
-try:
-    fname=input("Please enter text file name: ")
-    f=open(fname,"r")
-    for line in f:
-        data=line.split("|")    #split line with "|" ,It will return list of words
-        if(data[1]=="D"):       #to ignore Header and Trailer record
-            country=data[9]
-            customer_id=int(data[3])
-            try:                #if table already exist it will throw an exception
-                s="CREATE TABLE {}(Customer_Name VARCHAR(255) NOT NULL,Customer_ID VARCHAR(18) PRIMARY KEY,Customer_Open_Date DATE NOT NULL,Last_Consulted_Date DATE,Vaccination_Type CHAR(5),Doctor_Consulted CHAR(255),State CHAR(5),Country CHAR(5),Post_Code INT(5),Date_of_Birth DATE,Active_Customer CHAR(1))".format(country)
-                cur.execute(s)
-                mydb.commit()
-            except:
-                pass
-            finally:
-                s="INSERT INTO {} VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(country)
-                values=(data[2],customer_id,data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12])
-                cur.execute(s,values)
-                mydb.commit()
-    print("DATA inserted in Tabels succesfully :)")
-    f.close()          
-except:
-    print("Sorry!!! File not found")
-finally:   
-    mydb.close()
+def insert_data(data,mydb,cur):
+    country=data[9]
+    customer_id=int(data[3])
+    try:                #if table already exist it will throw an exception
+        s="CREATE TABLE {}(Customer_Name VARCHAR(255) NOT NULL,Customer_ID VARCHAR(18) PRIMARY KEY,\
+        Customer_Open_Date DATE NOT NULL,Last_Consulted_Date DATE,Vaccination_Type CHAR(5),\
+        Doctor_Consulted CHAR(255),State CHAR(5),Country CHAR(5),\
+        Post_Code INT(5),Date_of_Birth DATE,Active_Customer CHAR(1))".format(country)
+        cur.execute(s)
+        mydb.commit()
+    except:
+        pass
+    finally:
+        s="INSERT INTO {} VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(country)
+        values=(data[2],customer_id,data[4],data[5],data[6],data[7],\
+                data[8],data[9],data[10],data[11],data[12])
+        cur.execute(s,values)
+        mydb.commit()
+                    
+def read_and_insert_data(mydb,cur):
+    try:
+        fname=input("Please enter text file name: ")
+        f=open(fname,"r")
+        for line in f:
+            data=line.split("|")    #split line with "|" ,It will return list of words
+            if(data[1]=="D"):       #to ignore Header and Trailer record
+                insert_data(data,mydb,cur)
+            
+        print("DATA inserted in Tabels succesfully :)")
+        f.close()          
+    except:
+        print("Sorry!!! File not found")
+
+mydb=connect_sql()
+cur=create_database(mydb)
+read_and_insert_data(mydb,cur)
+mydb.close()
